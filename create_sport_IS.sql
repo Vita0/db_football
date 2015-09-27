42fa5eac36a4bc1d7244ce418a2b6dbb8a9450a1
Vita0 <bumbum0@ya.ru>
2015-09-27 15:19:40 +0300

    [*] fix table goals [+] add autoincrement all tables [+] add fell every tables

--- create_sport_IS.sql
+++ create_sport_IS.sql
@@ -7,52 +7,43 @@ connect 'E:\Program Files\Firebird\db\sport_IS.fdb'
 user 'SYSDBA' password 'masterkey';
 commit;
 
+
 CREATE DOMAIN pos_type
-AS varchar(20) CHECK (value IS NULL or VALUE IN 
+AS varchar(10) CHECK (value IS NULL or VALUE IN 
 	('Goalkeeper', 'Defender', 'Midfielder', 'Forward', 'Coach'));
 
 CREATE DOMAIN league_type
-AS varchar(20) CHECK (value IS NULL or VALUE IN
+AS varchar(10) CHECK (value IS NULL or VALUE IN
 	('World', 'Continent', 'Country', 'City'));
+
 
 CREATE TABLE players
 	(player_id INTEGER NOT NULL PRIMARY KEY,
-	 name VARCHAR(20) NOT NULL,
+	 name VARCHAR(16) NOT NULL,
 	 birthdate DATE,
 	 pos pos_type
 	);
 commit;
 
-CREATE GENERATOR gen_player_id;
-SET GENERATOR gen_player_id TO 0;
-set term !! ;
-CREATE TRIGGER PLAYERS_BI FOR players
-ACTIVE BEFORE INSERT POSITION 0
-AS
-BEGIN
-NEW.player_id = GEN_ID(gen_player_id, 1);
-END!!
-set term ; !!
-
 CREATE TABLE clubs
 	(club_id INTEGER NOT NULL PRIMARY KEY,
-	 name VARCHAR(20) NOT NULL,
+	 name VARCHAR(16) NOT NULL,
 	 birthdate DATE,
-	 nickname VARCHAR(20),
-	 country VARCHAR(20)
+	 nickname VARCHAR(17),
+	 country VARCHAR(16)
 	);
 commit;
 
 CREATE TABLE leagues
-	(leadues_id INTEGER NOT NULL PRIMARY KEY,
-	 name VARCHAR(20) NOT NULL,
+	(league_id INTEGER NOT NULL PRIMARY KEY,
+	 name VARCHAR(16) NOT NULL,
 	 kind league_type
 	);
 commit;
 
 CREATE TABLE seasons
 	(season_id INTEGER NOT NULL PRIMARY KEY,
-	 leadues_id INTEGER REFERENCES leagues(leadues_id),
+	 league_id INTEGER REFERENCES leagues(league_id),
 	 period VARCHAR(9)
 	);
 commit;
@@ -70,44 +61,105 @@ commit;
 
 CREATE TABLE goals
 	(goal_id INTEGER NOT NULL PRIMARY KEY,
+	 player_id INTEGER REFERENCES players(player_id),
 	 match_id INTEGER REFERENCES matches(match_id),
 	 goal_minute INTEGER,
-	 autogoal CHAR
+	 autogoal CHAR(1)
 	);
 commit;
 
 CREATE TABLE standings
 	(season_id INTEGER REFERENCES seasons(season_id),
 	 team_id INTEGER REFERENCES clubs(club_id),
-	 victories INTEGER,
-	 draws INTEGER,
-	 losses INTEGER,
-	 goals INTEGER,
-	 missed_goals INTEGER,
-	 diff_goals INTEGER
+	 victories SMALLINT,
+	 draws SMALLINT,
+	 losses SMALLINT,
+	 goals SMALLINT,
+	 missed_goals SMALLINT,
+	 diff_goals SMALLINT
 	);
 commit;
 
 
 
+CREATE GENERATOR gen_player_id;
+SET GENERATOR gen_player_id TO 0;
+set term !! ;
+CREATE TRIGGER PLAYERS_BI FOR players
+ACTIVE BEFORE INSERT POSITION 0
+AS
+BEGIN
+NEW.player_id = GEN_ID(gen_player_id, 1);
+END!!
+set term ; !!
 
+CREATE GENERATOR gen_club_id;
+SET GENERATOR gen_club_id TO 0;
+set term !! ;
+CREATE TRIGGER CLUBS_BI FOR clubs
+ACTIVE BEFORE INSERT POSITION 0
+AS
+BEGIN
+NEW.club_id = GEN_ID(gen_club_id, 1);
+END!!
+set term ; !!
 
+CREATE GENERATOR gen_league_id;
+SET GENERATOR gen_league_id TO 0;
+set term !! ;
+CREATE TRIGGER LEAGUES_BI FOR leagues
+ACTIVE BEFORE INSERT POSITION 0
+AS
+BEGIN
+NEW.league_id = GEN_ID(gen_league_id, 1);
+END!!
+set term ; !!
 
+CREATE GENERATOR gen_season_id;
+SET GENERATOR gen_season_id TO 0;
+set term !! ;
+CREATE TRIGGER SEASONS_BI FOR seasons
+ACTIVE BEFORE INSERT POSITION 0
+AS
+BEGIN
+NEW.season_id = GEN_ID(gen_season_id, 1);
+END!!
+set term ; !!
 
+CREATE GENERATOR gen_match_id;
+SET GENERATOR gen_match_id TO 0;
+set term !! ;
+CREATE TRIGGER MATCHES_BI FOR matches
+ACTIVE BEFORE INSERT POSITION 0
+AS
+BEGIN
+NEW.match_id = GEN_ID(gen_match_id, 1);
+END!!
+set term ; !!
 
+CREATE GENERATOR gen_goal_id;
+SET GENERATOR gen_goal_id TO 0;
+set term !! ;
+CREATE TRIGGER GOALS_BI FOR goals
+ACTIVE BEFORE INSERT POSITION 0
+AS
+BEGIN
+NEW.goal_id = GEN_ID(gen_goal_id, 1);
+END!!
+set term ; !!
 
-
-
-
-
-
-
-
-
-
-
-
-
+/*
+CREATE GENERATOR gen_standing_id;
+SET GENERATOR gen_standing_id TO 0;
+set term !! ;  STANDINGS
+CREATE TRIGGER STANDINGS_BI FOR standings
+ACTIVE BEFORE INSERT POSITION 0
+AS
+BEGIN
+NEW.standing_id = GEN_ID(gen_standing_id, 1);
+END!!
+set term ; !!
+*/
 
 
 
--- fell_sport_IS.sql
+++ fell_sport_IS.sql
@@ -3,8 +3,32 @@ connect 'E:\Program Files\Firebird\db\sport_IS.fdb'
 
 INSERT INTO players (name, birthdate, pos) 
 	VALUES ('Дзюба', '22-AUG-1988', 'Forward');
-
 INSERT INTO players (name, birthdate, pos) 
 	VALUES ('Кержаков', '10-JUN-1982', 'Forward');
+	
+INSERT INTO clubs (name, birthdate, nickname, country)
+	VALUES ('Зенит', '25-MAY-1925', 'Сине-Бело-Голубые', 'Россия');
+INSERT INTO clubs (name, birthdate, nickname, country)
+	VALUES ('Спартак', '18-APR-1922', 'Красно-Белые', 'Россия');
 
+INSERT INTO leagues (name, kind)
+	VALUES ('РФПЛ', 'Country');
 
+/*lid = (select league_id from leagues where name = 'РФПЛ');*/
+INSERT INTO seasons (league_id, period)
+	VALUES (1/*lid*/, '2015/2016');
+
+INSERT INTO matches (match_date, season_id, first_team_id, second_team_id, first_team_goals, second_team_goals)
+	VALUES('26-SEP-2015',1,2,1,2,2);
+
+INSERT INTO goals (match_id, player_id, goal_minute, autogoal)
+	VALUES(1,1,22,0);
+INSERT INTO goals (match_id, player_id, goal_minute, autogoal)
+	VALUES(1,2,41,0);
+
+INSERT INTO standings (season_id, team_id, victories, draws, losses, goals, missed_goals, diff_goals)
+	VALUES(1,1,0,1,0,2,2,0);
+INSERT INTO standings (season_id,team_id, victories, draws, losses, goals, missed_goals, diff_goals)
+	VALUES(1,1,0,1,0,2,2,0);
+
+	
