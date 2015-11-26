bac180b6e3815390926233a3644ab08e056aef0f
Vita0 <bumbum0@ya.ru>
2015-11-26 04:07:09 +0300

    [*] fix 2,3,4 labs

--- modify_sport_IS.sql
+++ modify_sport_IS.sql
@@ -1,53 +1,119 @@
-ALTER TABLE matches
-	ADD first_club_yellow SMALLINT; /*жЄлтые карточки*/
-ALTER TABLE matches
-	ADD second_club_yellow SMALLINT;
-	
-ALTER TABLE matches
-	ADD first_club_red SMALLINT; /*красные карточки*/
-ALTER TABLE matches
-	ADD second_club_red SMALLINT;
-	
+CREATE DOMAIN event_type
+AS varchar(11) CHECK (value IS NULL or VALUE IN 
+	('GOAL', 'YELLOW', 'RED', 'INJURY', 'REPLACEMENT'));
+							/* травма    замена */
+
+CREATE TABLE events
+	(player_id INTEGER REFERENCES players(player_id),
+	 match_id INTEGER REFERENCES matches(match_id),
+	 event_minute SMALLINT,
+	 event event_type,
+	 ag_io CHAR(1)
+	);
+/* ag_io - GOAL: 1-autogoal, 0-goal;  YELLOW,RED: NULL; INJURY: 1-нанЄс травму, 0-получил травму; REPLACEMENT: 1-зашЄл на поле, 0-вышел */
+
+insert into events (player_id, match_id, event_minute, ag_io, event)
+	select player_id, match_id, goal_minute, autogoal, 'GOAL' from goals;
+
+DROP trigger GOALS_BI;	
+DROP generator gen_goal_id;
+/*drop view alterPointsInRFPL15_16;
+drop view selectSumOfGoalsRFPL15_16;
+drop view selectGoalsPlayers;
+drop procedure ins_goal;*/
+commit;
+DROP table goals;
+commit;
+
 ALTER TABLE matches
 	ADD first_club_percent SMALLINT; /*владение м€чом*/
-	
-ALTER TABLE matches
-	ADD first_club_pressure SMALLINT; /*владение на чужой половине (% от своего владени€, а не от времени всего матча)*/
-ALTER TABLE matches
-	ADD second_club_pressure SMALLINT;
 
 ALTER TABLE matches
 	ADD first_club_shorts SMALLINT; /*удары*/
 ALTER TABLE matches
 	ADD second_club_shorts SMALLINT;
 
-ALTER TABLE matches
-	ADD first_club_shorts_in_target SMALLINT; /*удары в створ*/
-ALTER TABLE matches
-	ADD second_club_shorts_in_target SMALLINT;
+/*ALTER TABLE matches
+	ADD first_club_shorts_in_target SMALLINT;*/ /*удары в створ*/
+/*ALTER TABLE matches
+	ADD second_club_shorts_in_target SMALLINT;*/
 
-
+ALTER TABLE matches
+	DROP first_club_goals;
+ALTER TABLE matches
+	DROP second_club_goals;
 	
+ALTER TABLE matches
+	ADD first_club_goals SMALLINT;
+ALTER TABLE matches
+	ADD second_club_goals SMALLINT;
+	
+commit;
+
+
+INSERT INTO clubs (name, birthdate, nickname, country)
+	VALUES ('CSKA', '5-AUG-1937', 'Armeytci', 'Russia');
+INSERT INTO players (name, birthdate, pos) 
+	VALUES ('Natho', '2-JUL-1983', 'Midfielder');
+INSERT INTO players (name, birthdate, pos) 
+	VALUES ('Berezuzkiy V', '7-MAY-1987', 'Defender');
+INSERT INTO players (name, birthdate, pos) 
+	VALUES ('Movsisyan', '30-OCT-1991', 'Forward');
+INSERT INTO matches (match_date, season_id, first_club_id, second_club_id)
+	VALUES('30-NOV-2015',1,3,1);
+INSERT INTO matches (match_date, season_id, first_club_id, second_club_id)
+	VALUES('30-NOV-2015',1,2,3);
+
+INSERT INTO club_players_list_history (club_id, player_id, date_begin, date_end)
+	VALUES (1, 3, '15-MAY-2013', NULL);
+INSERT INTO club_players_list_history (club_id, player_id, date_begin, date_end)
+	VALUES (3, 4, '3-OCT-2015', NULL);
+INSERT INTO club_players_list_history (club_id, player_id, date_begin, date_end)
+	VALUES (3, 5, '2-NOV-2000', NULL);
+INSERT INTO club_players_list_history (club_id, player_id, date_begin, date_end)
+	VALUES (2, 6, '1-JUN-2012', NULL);
+	
+INSERT INTO current_club_players_list (club_id, player_id)
+	VALUES (1, 3);
+INSERT INTO current_club_players_list (club_id, player_id)
+	VALUES (3, 4);
+INSERT INTO current_club_players_list (club_id, player_id)
+	VALUES (3, 5);
+INSERT INTO current_club_players_list (club_id, player_id)
+	VALUES (2, 6);
+	
+INSERT INTO events (player_id, match_id, event_minute, event, ag_io)
+	VALUES (4, 2, 7, 'GOAL', 0);
+INSERT INTO events (player_id, match_id, event_minute, event, ag_io)
+	VALUES (1, 2, 12, 'GOAL', 0);
+INSERT INTO events (player_id, match_id, event_minute, event, ag_io)
+	VALUES (6, 3, 79, 'GOAL', 0);
+
 /*
-CREATE TABLE yellow_cards
-	(match_id INTEGER REFERENCES matches(match_id),
-	 player_id INTEGER REFERENCES players(player_id),
-	 card_minute SMALLINT
-	);
-commit;
-
-CREATE TABLE red_cards
-	(match_id INTEGER REFERENCES matches(match_id),
-	 player_id INTEGER REFERENCES players(player_id),
-	 card_minute SMALLINT
-	);
-commit;
-
-INSERT INTO yellow_cards (match_id, player_id, card_minute)
-	VALUES(1,1,25);
-INSERT INTO yellow_cards (match_id, player_id, card_minute)
-	VALUES(1,1,90);
-	
-INSERT INTO red_cards (match_id, player_id, card_minute)
-	VALUES(1,1,90);
+spartak 0 2 zenit
+cska    1 1 zenit
+spartak 1 0 cska
 */
+delete from standings where club_id > 0;
+INSERT INTO standings (season_id, club_id, victories, draws, losses, goals, missed_goals, diff_goals, points)
+	VALUES(1,1,1,1,0,3,1,2,4);
+INSERT INTO standings (season_id, club_id, victories, draws, losses, goals, missed_goals, diff_goals, points)
+	VALUES(1,2,1,0,1,1,2,-1,3);
+INSERT INTO standings (season_id, club_id, victories, draws, losses, goals, missed_goals, diff_goals, points)
+	VALUES(1,3,0,1,1,1,2,-1,1);
+	
+UPDATE matches SET first_club_goals = 0, second_club_goals = 2 where match_id = 1;
+UPDATE matches SET first_club_goals = 1, second_club_goals = 1 where match_id = 2;
+UPDATE matches SET first_club_goals = 1, second_club_goals = 0 where match_id = 3;
+	
+	
+insert into events (player_id, match_id, event_minute, event, ag_io)
+	VALUES (1, 1, 15, 'YELLOW', 0);
+insert into events (player_id, match_id, event_minute, event, ag_io)
+	VALUES (5, 2, 90, 'RED', 0);
+	
+
+/*ALTER TABLE matches
+	ADD first_club_pressure SMALLINT;*/ /*владение на чужой половине (% от своего владени€, а не от времени всего матча)*/
+/*ALTER TABLE matches
+	ADD second_club_pressure SMALLINT;*/
--- sql-dml-std.sql
+++ sql-dml-std.sql
@@ -4,7 +4,7 @@ CREATE VIEW clubsSel AS SELECT * FROM clubs;
 CREATE VIEW leaguesSel AS SELECT * FROM leagues;
 CREATE VIEW seasonsSel AS SELECT * FROM seasons;
 CREATE VIEW matchesSel AS SELECT * FROM matches;
-CREATE VIEW goalsSel AS SELECT * FROM goals;
+CREATE VIEW eventsSel AS SELECT * FROM events;
 CREATE VIEW standingsSel AS SELECT * FROM standings;
 CREATE VIEW club_players_list_historySel AS SELECT * FROM club_players_list_history;
 CREATE VIEW current_club_players_listSel AS SELECT * FROM current_club_players_list;
@@ -55,45 +55,111 @@ SQL>
 CREATE VIEW alterPointsInRFPL15_16 as select club_id, (points+2*diff_goals) as alterPoints from standings where season_id like 1;
 commit;
 
+/*
+SQL> select * from alterPointsInRFPL15_16;
+
+     CLUB_ID           ALTERPOINTS
+============ =====================
+           1                     8
+           2                     1
+           3                    -1
+*/
+
 /*4 Сделайте выборку всех данных с сортировкой по нескольким полям*/
 
 /*выборка с=отсортирована по позиции, имени, и дате рождения*/
 CREATE VIEW selectPlayersSort as select * from players order by pos asc, name asc, birthdate;
 commit;
 
+/*
+SQL> select * from selectPlayersSort;
+
+   PLAYER_ID NAME               BIRTHDATE POS
+============ ================ =========== ==========
+           5 Berezuzkiy V     1987-05-07  Defender
+           1 Dzuba            1988-08-22  Forward
+           2 Kerjakov         1982-06-10  Forward
+           6 Movsisyan        1991-10-30  Forward
+           3 Lodigin          1990-06-01  Goalkeeper
+           4 Natho            1983-07-02  Midfielder
+*/
+
 /*5 Создайте запрос, вычисляющий несколько совокупных характеристик таблиц*/
 
 /*подсчёт голов в сезоне РФПЛ 15-16*/
 CREATE VIEW selectSumOfGoalsRFPL15_16 as select sum(goals) as sumOfGoals from standings where season_id like 1;
 commit;
+/*
+SQL> select * from selectSumOfGoalsRFPL15_16;
+
+           SUMOFGOALS
+=====================
+                    5
+*/
+
 
 /*6 Сделайте выборку данных из связанных таблиц (не менее двух примеров)*/
 
 /*Команда - количество очков*/
 CREATE VIEW selectClubsAndPoints as select Clubs.name, Standings.points from Clubs, Standings where Clubs.club_id = Standings.club_id;
 commit;
+/*
+SQL> select * from selectClubsAndPoints;
+
+NAME              POINTS
+================ =======
+Zenit                  4
+Spartak                3
+CSKA                   1
+*/
+
 
 /*команда 1 команда 2 счёт*/
-/*select c1.name as club1, c2.name as club2, m.first_club_goals, m.second_club_goals
+CREATE VIEW selectMatchesGoals as select c1.name as club1, c2.name as club2, m.first_club_goals, m.second_club_goals
     from Matches m inner join clubs c1 on c1.club_id = m.first_club_id
     inner join clubs c2 on c2.club_id = m.second_club_id;
-commit;*/
-
-/*голы: игрок дата_матча минута_матча*/
-CREATE VIEW selectGoalsPlayers as select Players.name, Matches.match_date, Goals.goal_minute from Players, Matches, Goals where Goals.player_id = Players.player_id and Goals.match_id = Matches.match_id;
 commit;
+/*
+SQL> select * from selectMatchesGoals;
+
+CLUB1            CLUB2            FIRST_CLUB_GOALS SECOND_CLUB_GOALS
+================ ================ ================ =================
+Spartak          Zenit                           0                 2
+Spartak          CSKA                            1                 0
+CSKA             Zenit                           1                 1
+*/
 
 /*7 Создайте запрос, рассчитывающий совокупную характеристику с использованием группировки, наложите ограничение на результат группировки*/
 
 /*количество игроков каждой позиции*/
 CREATE VIEW selectCountOfPlayersByPos as select Players.pos as "position", COUNT(Players.pos) as "count" from Players group by Players.pos;
 commit;
+/*
+SQL> select * from selectCountOfPlayersByPos;
+
+position          count
+========== ============
+Defender              1
+Forward               3
+Goalkeeper            1
+Midfielder            1
+*/
 
 /*8 Придумайте и реализуйте пример использования вложенного запроса*/
 
 /*показать игроков Зенита*/
 CREATE VIEW selectPlayersOfZenit as select * from players where player_id in (select player_id from current_club_players_list where club_id = (select club_id from clubs where name = 'Zenit'));
 commit;
+
+/*
+SQL> select * from selectPlayersOfZenit;
+
+   PLAYER_ID NAME               BIRTHDATE POS
+============ ================ =========== ==========
+           1 Dzuba            1988-08-22  Forward
+           2 Kerjakov         1982-06-10  Forward
+           3 Lodigin          1990-06-01  Goalkeeper
+*/
 
 /*9 С помощью оператора INSERT добавьте в каждую таблицу по одной записи*/
 
@@ -127,10 +193,10 @@ begin
 	insert into matches (match_date, season_id, first_club_id, second_club_id, first_club_goals, second_club_goals) values (:d, :sid, :fcid, :scid, :fcg, :scg);
 end;
 
-create procedure ins_goal(pid INTEGER, "mid" INTEGER, gm SMALLINT, ag CHAR(1))
+create procedure ins_event(pid INTEGER, "mid" INTEGER, em SMALLINT, e event_type, ag_io CHAR(1))
 as
 begin
-	insert into goals (match_id, player_id, goal_minute, autogoal) values (:pid, :"mid", :gm, :ag);
+	insert into events (player_id, match_id, event_minute, event, ag_io) values (:pid, :"mid", :em, :e, :ag_io);
 end;
 
 create procedure ins_club_in_season(sid INTEGER, cid INTEGER, v SMALLINT, d SMALLINT, l SMALLINT, g SMALLINT, mg SMALLINT, dg SMALLINT, p SMALLINT)
@@ -155,15 +221,14 @@ commit;
 
 /*
 execute procedure ins_player('Shatov', '22-AUG-1992', 'Midfielder');
-execute procedure ins_club('CSKA', '25-MAY-1950', 'Armetci', 'Russia');
+
 execute procedure ins_league('EPL', 'Country');
-execute procedure ins_season(2/*lid*/, '2015/2016');
-execute procedure ins_match('25-OCT-2015',1/*Zenit*/,100003/*CSKA*/,1,7,0);
-execute procedure ins_goal(2, 100004, 87, 0);
-execute procedure ins_club_in_season(1,100003,0,1,0,2,2,0,1);
-execute procedure ins_player_in_club_history(1, 100004/*Shatov*/, '20-JUL-2015', NULL);
-execute procedure ins_player_in_club_curr(1, 100004);
+execute procedure ins_season(2, '2015/2016');
+execute procedure ins_event(2, 2, 90, 'INJURY', 0);
+execute procedure ins_player_in_club_history(1, 7, '20-JUL-2015', NULL);
+execute procedure ins_player_in_club_curr(1, 7);
 */
+/*ins_event not work?*/
 
 /*10 С помощью оператора UPDATE измените значения нескольких полей у всех записей, отвечающих заданному условию*/
 
@@ -187,7 +252,8 @@ begin
 end;
 
 /*
-execute procedure deleteYoungPlayer
+execute procedure ins_player('young_man', '2-AUG-2015', 'Midfielder');
+execute procedure deleteYoungPlayer;
 */
 
 
@@ -199,6 +265,7 @@ begin
 	delete from Leagues where league_id not in (select league_id from Seasons);
 end;
 /*
+execute procedure ins_league('FPL', 'Country');
 execute procedure deleteNotUseLeagues;
 */
 
@@ -209,38 +276,55 @@ execute procedure deleteNotUseLeagues;
 */
 
 /* 1 Вывести 10 самых результативных матчей за всю историю. */
-create view selectMoreResultsMatches as select first 10 * from matches order by (first_club_goals+second_club_goals) desc;
+create or alter view selectMoreResultsMatches as
+select c1.name as club1, c2.name as club2, m.first_club_goals, m.second_club_goals, m.match_date
+from (select * from
+         (
+          select first 10 match_id as mid, count(*) goal_count
+               from events where event = 'GOAL' group by match_id
+               order by goal_count desc) as tmp
+          join matches on matches.match_id = tmp.mid
+      ) m
+
+    inner join clubs c1 on c1.club_id = m.first_club_id
+    inner join clubs c2 on c2.club_id = m.second_club_id;
 
 /*
 SQL> select * from selectMoreResultsMatches;
 
-    MATCH_ID  MATCH_DATE    SEASON_ID FIRST_CLUB_ID SECOND_CLUB_ID FIRST_CLUB_GOALS SECOND_CLUB_GOALS FIRST_CLUB_YELLOW SECOND_CLUB_YELLOW FIRST_CLUB_RED SECOND
-_CLUB_RED FIRST_CLUB_PERCENT FIRST_CLUB_PRESSURE SECOND_CLUB_PRESSURE FIRST_CLUB_SHORTS SECOND_CLUB_SHORTS FIRST_CLUB_SHORTS_IN_TARGET SECOND_CLUB_SHORTS_IN_TAR
-GET
-============ =========== ============ ============= ============== ================ ================= ================= ================== ============== ======
-========= ================== =================== ==================== ================= ================== =========================== =========================
-===
-           2 2015-10-25             1        100003              1                7                 0            <null>             <null>         <null>
-   <null>             <null>              <null>               <null>            <null>             <null>                      <null>                       <nu
-ll>
-           1 2015-09-26             1             2              1                2                 2            <null>             <null>         <null>
-   <null>             <null>              <null>               <null>            <null>             <null>                      <null>                       <nu
-ll>
+CLUB1            CLUB2            FIRST_CLUB_GOALS SECOND_CLUB_GOALS  MATCH_DATE
+================ ================ ================ ================= ===========
+Spartak          Zenit                           0                 2 2015-09-26
+CSKA             Zenit                           1                 1 2015-11-30
+Spartak          CSKA                            1                 0 2015-11-30
 */
 
 /* 2 Вывести 5 команд с наибольшим количеством побед за выбранный период. */
-create view selectTopClubs as select name from clubs where club_id in (select first 5 club_id from standings order by points);
+create or alter view selectTopClubs as
+    select clubs.name, wins from
+        (
+        select first 5 cid, count(cid) wins from
+            (
+            select first_club_id cid from matches
+                where matches.match_date between '01-SEP-2015' and '31-DEC-2015'
+                  and matches.first_club_goals > matches.second_club_goals
+            union all
+            select second_club_id cid from matches
+                where matches.match_date between '01-SEP-2015' and '31-DEC-2015'
+                  and matches.first_club_goals < matches.second_club_goals
+            )
+        group by cid order by wins desc
+        )
+        , clubs
+    where clubs.club_id = cid;
 
 /*
 SQL> select * from selectTopClubs;
 
-NAME
-================
-Zenit
-Spartak
-CSKA
-
-SQL>
+NAME                     WINS
+================ ============
+Zenit                       1
+Spartak                     1
 */
 
 /* 3 Удалить неиспользуемые лиги. */
